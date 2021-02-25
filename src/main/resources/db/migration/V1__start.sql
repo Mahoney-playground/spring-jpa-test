4df9d6948b3a5b7ae0a950077f5b3e3000947e5d
Robert Elliot <rob@lidalia.org.uk>
2021-02-25 07:52:55 +0000

    Make it clear the view has an identical structure to the table

--- src/main/resources/db/migration/V1__start.sql
+++ src/main/resources/db/migration/V1__start.sql
@@ -5,12 +5,13 @@ create table data_snapshot(
     primary key (id, revision)
 );
 
-create view latest_data_snapshot(id, revision, data) as
-select data_snapshot.id, data_snapshot.revision, data_snapshot.data
+create view latest_data_snapshot as
+select data_snapshot.*
 from data_snapshot
 join (
   select id, max(revision) as latest_revision
   from data_snapshot
   group by id
 ) latest_snapshot
-on data_snapshot.id = latest_snapshot.id and data_snapshot.revision = latest_snapshot.latest_revision;
+on data_snapshot.id = latest_snapshot.id
+and data_snapshot.revision = latest_snapshot.latest_revision;
